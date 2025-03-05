a661d403da1d98e831af0371ddfc88da5aa23cd9
Paul Calzado <pafcalzado@gmail.com>
2025-03-05 13:32:49 +0800

    Fix blank GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/ocr-app/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
 });
