0e12fdeeaf478a45e5272997193cb9d2503ae65c
Paul Calzado <pafcalzado@gmail.com>
2025-03-05 14:58:12 +0800

    Fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
+  base: '/ocr-app/',
 });
